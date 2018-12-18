90b756e8687e4b3720edfc7003f8ab5e07b88bc1
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-18 00:57:47 +0000

    used Array.isArray, Array.from, Array.from(titles).forEach

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+var titles = document.getElementsByClassName('title');  //
 
-var books = document.querySelector('#book-list li .name');
-console.log(books);
+console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles)));
 
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
-
-Array.from(books).forEach(function(book){
-  console.log(book);
+Array.from(titles).forEach(function(item) {
+    console.log(item);
 });
+
+
+
+
+
+
+
