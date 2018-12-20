a55a54d28590cf6b72b7e67b92826caef9e64d89
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-20 03:27:20 +0000

    in depth lesson on event listeners

--- app.js
+++ app.js
@@ -1,42 +1,83 @@
-// div = header = form //these are sibling element. They operate at the same level
+//DOM EVENTS AND REMOVING CONTENT FROM THE DOM
+
+//an event is the action happening on the browser. When you press the 'delete' button thats a click event. When you hit the space inside the search bar and it shows that line that means its ready to write text, that's a key-press event. When you hit 'add' to add a book to the list, that's a submit event.
+
+//we add event-listeners to the elements we want to listen to events on
+
+var h2 = document.querySelector('#book-list h2')
+
+//there are many kinds of events. for example I could have put 'keypress' instead of 'click'
+//the function inside of the the addEventListener tells the code what to do once it's 'heard' something. It is a callback function
+//'action' is a made up name for my parameter
+
+h2.addEventListener('click', function (action) {
+  console.log(action.target)
+  console.log(action)
+})  
+
+//the code above has to be entered into the inspect-console so you can see what happens when you click on h2
 
 
-var bookList = document.querySelector('#book-list');
-
-console.log('book-list next sibling is:', bookList.nextSibling);    //console says #text is the next sibling. this is #text node that shows line-breaks
-console.log('book-list next element sibling is:', bookList.nextElementSibling);    // you get <form id="add-book">...</form> with all the form (search bar) stuff nested
+// -------------------------------------------------------
 
 
-console.log('book-list previous sibling is:', bookList.previousSibling);    //console says #text is the next sibling. this is #text node that shows line-breaks
-console.log('book-list previous element sibling is:', bookList.previousElementSibling);    // you get <header>...</header> with all the header stuff nested inside
+//book-list is the DIV id where the list of books is contained. delete is the class for all of he buttons
+//we cant do btns.addEventListener() because delete buttons are a collection of elements, not a single one
+//we want to loop through them and add an event listener to each one, which means we need to turn this collection into an array
+//when click happens the code removes the <li> so that the title is deleted
 
 
-// ---------------------------------------------------------
+var btns = document.querySelectorAll('#book-list.delete')
 
-// GETTING TRICKIER NOW!
+Array.from(btns).forEach(function(btns) {
+  btns.addEventListener('click', function(action) {
+    
+    var li = action.target.parentElement    //what we want to happen when click happens
+    li.parentNode.removeChild(li)              //parent of <li> is <ul>, which is why we're telling <li>'s parent to remove it's child
+    
+  })
+})
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for school';
+// -----------------------------------------------
+//GOTTA BE HONEST, I JUST COPY/PASTED BELOW WHAT WAS ALREADY THERE
+ // prevent default behaviour
 
-//bookList.previousElementSibling references the #book-list (this is our starting point. We know <header> is a sibling)
-//querySelector is QUERYING everything inside of <header>
-//querySelector('p') specifies that we're grabbing this particular <p> within the <header> 
-// .innerHTML tells the computer that we're messing around with the text contained within this p tag
-// += means 'keep what's already there but also add this in'
-// <br/> is another line. (i think?)
+const link = document.querySelector('#page-banner a');
 
-
-
-
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
 
 
 
 
 
-// const bookList = document.querySelector('#book-list');
 
-// console.log('#book-list next sibling:', bookList.nextSibling);
-// console.log('#book-list next element sibling:', bookList.nextElementSibling);
-// console.log('#book-list previous sibling:', bookList.previousSibling);
-// console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-// bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+
+
+
+
+
+// const listItems = document.querySelectorAll('#book-list ul li');
+
+// Array.from(listItems).forEach(function(item){
+//   item.addEventListener('click', (e) => {
+
+//     const li = e.target.parentElement;
+//     console.log('child element to remove:', li);
+//     console.log('parent element to remove child from:', li.parentElement);
+//     li.parentNode.removeChild(li);
+
+//   });
+// });
+
+// // prevent default behaviour
+
+// const link = document.querySelector('#page-banner a');
+
+// link.addEventListener('click', function(e){
+//   e.preventDefault();
+//   console.log('Navigation to', e.target.textContent, 'was prevented');
+// });
