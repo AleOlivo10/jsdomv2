a5ce9caaa0b12f1c58c0c1453fba03863d7c5abe
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-12-20 02:24:36 +0000

    very thorough tutorial on running through sibling elements and changing them to create new things in the HTML

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-//DOM TRAVERSAL (PARENT AND CHILD)
+// div = header = form //these are sibling element. They operate at the same level
+
 
 var bookList = document.querySelector('#book-list');
 
-console.log('the parent node is:', bookList.parentNode); // you get| the parent node is: <div id="wrapper">...</div>  | containing all the nested stuff inside of "..."
-//                                                        <div id="wrapper"> is the direct parent of #book-list
-
-//.parentElement and .parentNode are two ways to do the same thing and get the same result
-
-console.log('the parent element is:', bookList.parentElement);                //is the same way to ask the above line. gives the same answer
+console.log('book-list next sibling is:', bookList.nextSibling);    //console says #text is the next sibling. this is #text node that shows line-breaks
+console.log('book-list next element sibling is:', bookList.nextElementSibling);    // you get <form id="add-book">...</form> with all the form (search bar) stuff nested
 
 
-console.log('the parent element is:', bookList.parentElement.parentElement);  //gives the parent of the parent.
-//                                                                            The parent of <div id="wrapper">...</div> is <body>...</body>
+console.log('book-list previous sibling is:', bookList.previousSibling);    //console says #text is the next sibling. this is #text node that shows line-breaks
+console.log('book-list previous element sibling is:', bookList.previousElementSibling);    // you get <header>...</header> with all the header stuff nested inside
 
 
+// ---------------------------------------------------------
 
-//PARENTS ABOVE
-//----------------------------
-//CHILDREN BELOW
+// GETTING TRICKIER NOW!
 
-console.log(bookList.childNodes); //you get a node list--> NodeList(5) [text, h2.title, text, ul, text] | is too detailed and not ideal. gives info on line breaks
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for school';
 
-console.log(bookList.children); //you get HTML collection--> HTMLCollection(2) [h2.title, ul] | is best because it gives only pertinent info on children
-
-
-
-
-
-
-
+//bookList.previousElementSibling references the #book-list (this is our starting point. We know <header> is a sibling)
+//querySelector is QUERYING everything inside of <header>
+//querySelector('p') specifies that we're grabbing this particular <p> within the <header> 
+// .innerHTML tells the computer that we're messing around with the text contained within this p tag
+// += means 'keep what's already there but also add this in'
+// <br/> is another line. (i think?)
 
 
 
@@ -40,22 +34,9 @@ console.log(bookList.children); //you get HTML collection--> HTMLCollection(2) [
 
 // const bookList = document.querySelector('#book-list');
 
-// console.log('book list parent element:', bookList.parentElement);
-// console.log('book list parent node:', bookList.parentNode);
+// console.log('#book-list next sibling:', bookList.nextSibling);
+// console.log('#book-list next element sibling:', bookList.nextElementSibling);
+// console.log('#book-list previous sibling:', bookList.previousSibling);
+// console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-// console.log('all node children:');
-// Array.from(bookList.childNodes).forEach(function(node){
-//   console.log(node);
-// });
-
-// console.log('all element children:');
-// Array.from(bookList.children).forEach(function(node){
-//   console.log(node);
-// });
-
-// const titles = bookList.querySelectorAll('.name');
-
-// console.log('Book titles:');
-// Array.from(titles).forEach(function(title){
-//   console.log(title.textContent);
-// });
+// bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
